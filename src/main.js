0f962f161cb0bb032a0f26b2c90e808dc8bc68b7
ivan.liu <403704409@qq.com>
2016-10-03 21:48:36 +0800

    add loading

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import App from './App.vue'
+
 import Element from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import './assets/css/common.css'
 Vue.use(Element)
 
-import { sync } from 'vuex-router-sync'
-import router  from './route/router.js'
-import store from './store'
-// TODO: 升级vuex-router-sync到2.0
-sync(store, router)
+// somthing sync
+setTimeout(function() {
+	require(['./init.js'], function(){
+		setTimeout(()=>{
+			let loading = document.getElementById('loading')
+			loading.className = 'hide'
+			setTimeout(() => {
+				loading.remove()
+			}, 1000)
+		}, 1000)
+	})
+}, 300)
 
 
-const app = new Vue({
-	el: '#app',
-	router,
-	store,
-	render: h => h(App)
-})
+console.log(123)
